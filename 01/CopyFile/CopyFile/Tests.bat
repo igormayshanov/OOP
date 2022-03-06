24b676aac7b4ceb981c26e577bdf3e938d7fb2b5
igormaushanov <pogopios@gmail.com>
2022-03-07 00:07:19 +0300

    add tests

--- 01/CopyFile/CopyFile/Tests.bat
+++ 01/CopyFile/CopyFile/Tests.bat
@@ -11,13 +11,31 @@ if %MyProgram%=="" (
 
 REM Copy empty file
 %MyProgram% empty.txt "%TEMP%\output.txt" || goto err
-fc empty.txt "%TEMP%\output.txt" || goto err
+fc empty.txt "%TEMP%\output.txt" > nul  || goto err
+echo Test 1 passed
 
+REM Copy non empty file
+%MyProgram% NonEmptyFile.txt "%TEMP%\output.txt" || goto err
+fc NonEmptyFile.txt "%TEMP%\output.txt" > nul || goto err
+echo Test 2 passed
+
+REM Copy missing file should fail
+%MyProgram% MissingFile.txt "%TEMP%\output.txt" && goto err
+echo Test 3 passed
+
+REM Is output file is not specfied, program mast fail
+%MyProgram% MissingFile.txt && goto err
+echo Test 4 passed
+
+REM Is input and output file are not specfied, program mast fail
+%MyProgram% && goto err
+echo Test 5 passed
 
 REM Тесты прошли успешно
+echo All tests passed successfuly
 exit /B 0
 
 REM Сюда будем попадать в случае ошибки
 :err
 echo Test failed
-exit /B 1
+exit /B 1 
